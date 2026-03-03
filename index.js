5f2f5b1e0637115d602a6370b9eba8b032b13da4
Alexei Zhirnov <alexeizhirnov06@gmail.com>
2026-03-03 21:10:34 +0500

    5?

--- index.js
+++ index.js
@@ -39,32 +39,30 @@ function getTodosFromFile(fileInfo) {
 
 function getUser(username) {
     let total = getEntries()[0];
-    let result = []
+    let result = [];
     for (const entry of total) {
         if (entry[1].toLowerCase() === username.toLowerCase()) {
-            result.push(entry[0])
+            result.push(entry[0]);
         }
     }
     return result;
 }
 
 function getEntries() {
-    let total = []
-    let nameless = []
+    let total = [];
+    let nameless = [];
     const re = /\/\/ TODO ([ \w]+);( ){0,1}?([-\d]+);( ){0,1}(.+)/;
     let todos = getAllTodos(files);
     for (const string of todos) {
         let result = string.match(re);
-        if (result === null || result === undefined){
+        if (result === null || result === undefined) {
             nameless.push(string);
-        }
-        else {
+        } else {
             total.push(result);
         }
     }
     return [total, nameless];
 }
-
 
 function sortBy(command) {
     switch (command) {
@@ -72,16 +70,16 @@ function sortBy(command) {
             break;
         }
         case "user": {
-            return sortByUser()
+            return sortByUser();
         }
         case "date": {
-            break;
+            return sortByDate();
         }
     }
 }
 
 function sortByUser() {
-    let result = []
+    let result = [];
     let [allEntries, allNameless] = getEntries();
     let allNames = new Map();
     for (const entry of allEntries) {
@@ -103,9 +101,41 @@ function sortByUser() {
     return result;
 }
 
+function sortByDate() {
+    let result = [];
+    let [allEntries, allNamelessDateless] = getEntries();
+
+    let allDatesMap = new Map();
+
+    for (const entry of allEntries) {
+        const date = entry[3];
+        const re = /(\d+)-(\d+)-(\d+)/;
+        const [year, month, day] = date.match(re).map((el) => Number(el));
+        const correctDate = new Date(year, month - 1, day);
+        if (!allDatesMap.has(correctDate)) {
+            allDatesMap.set(correctDate, []);
+        }
+        let value = allDatesMap.get(correctDate);
+        value.push(entry[0]);
+        allDatesMap.set(correctDate, value);
+    }
+
+    let Keys = Array(allDatesMap.keys());
+    let sortedKeys = Keys.sort((a, b) => b - a);
+    for (key of sortedKeys){
+        for (value of allDatesMap[key]){
+            result.push(value);
+        }
+    }
+    return result.concat(allNamelessDateless);
+    const sortedEntries = [...myMap.entries()].sort(([keyA], [keyB]) => {
+    // Numeric comparison: a - b for ascending order
+    return keyA - keyB;
+});
+}
 
 function processCommand(command) {
-    switch (command.split(' ')[0]) {
+    switch (command.split(" ")[0]) {
         case "exit":
             process.exit(0);
             break;
@@ -123,14 +153,13 @@ function processCommand(command) {
             }
             break;
         }
-        case "user":{
-            let userName = command.split(' ')[1];
+        case "user": {
+            let userName = command.split(" ")[1];
             console.log(getUser(userName));
             break;
         }
-        case "sort":
-        {
-            console.log(sortBy(command.split(' ')[1]))
+        case "sort": {
+            console.log(sortBy(command.split(" ")[1]));
             break;
         }
         default:
