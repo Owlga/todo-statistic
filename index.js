0c1355830fc1323ba6192a01d9a2d259dff7b6a3
Owlga <olgau2ekb@gmail.com>
2026-03-03 19:59:08 +0500

    1-3

--- index.js
+++ index.js
@@ -11,11 +11,52 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function getAllTodos(fileInfo) {
+    let result = []
+    for (const file of fileInfo) {
+        const fileTodos = getTodosFromFile(file);
+        if (fileTodos) {
+            result = result.concat(fileTodos);
+        }
+    }
+    return result;
+}
+
+function getTodosFromFile(fileInfo) {
+    let result = [];
+    for (const string of fileInfo.split('\n')) {
+        if (string === null || string === undefined) {
+            continue;
+        }
+        const regExp = /\/\/ TODO .*/
+        const match = string.match(regExp);
+        if (match && match.length >= 1) {
+            result.push(match[0]);
+        }
+    }
+    return result;
+}
+
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show': {
+            console.log(getAllTodos(files));
+            console.log('showing');
+            break;
+        }
+        case 'important': {
+            let strings = getAllTodos(files);
+            for (let string of strings) {
+                if (string.includes('!')) {
+                    console.log(string);
+                }
+            }
+            break;
+        }
         default:
             console.log('wrong command');
             break;
